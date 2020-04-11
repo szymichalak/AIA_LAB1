dc2831deae25955656937429e4762d22f066eacf
inf136769 <szymonmichalak97@gmail.com>
2020-04-11 18:28:48 +0200

    Updates

---
+++ precache-manifest.87b7a4700483e7a12befeca99555018f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7cd14be9eec0d2960332ba8371a0db",
+    "url": "/AIA_LAB1/index.html"
+  },
+  {
+    "revision": "6844e7ba5a28ab473c8c",
+    "url": "/AIA_LAB1/static/css/main.69cb425f.chunk.css"
+  },
+  {
+    "revision": "181674c9f52f8c332b77",
+    "url": "/AIA_LAB1/static/js/2.48f35468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AIA_LAB1/static/js/2.48f35468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6844e7ba5a28ab473c8c",
+    "url": "/AIA_LAB1/static/js/main.9613407e.chunk.js"
+  },
+  {
+    "revision": "bf6c0d5f9a4ba54a452a",
+    "url": "/AIA_LAB1/static/js/runtime-main.68babd85.js"
+  }
+]);
